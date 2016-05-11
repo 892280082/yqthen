6b13adf3cd4b3f5652b19c7f7b94c8eb1fb12e01
892280082 <892280082@qq.com>
2016-05-11 21:20:39 +0800

    change test go

--- test/testGo.js
+++ test/testGo.js
@@ -7,36 +7,55 @@ then((next)=>{
 	console.log('testGo1 start');
 	next();
 
-}).go((next)=>{
+})
+.go((next)=>{
 
 	setTimeout(()=>{
 		console.log('1func 100ms');
 		next();
 	},100);
 
-}).go((next)=>{
+})
+.go((next)=>{
 
 	setTimeout(()=>{
 		console.log('2func 200ms');
 		next();
 	},200);
 	
-}).go((next)=>{
+})
+.go((next)=>{
 	setTimeout(()=>{
 		console.log('3func 300ms');
 		next();
 	},300);
-}).then((next)=>{
+})
+.then((next)=>{
 	setTimeout(()=>{
 		console.log('then1');
 		next();
 	},0);
-	next();
-}).then((next)=>{
+})
+.go((next)=>{
+
+	setTimeout(()=>{
+		console.log('4func 100ms');
+		next();
+	},100);
+	
+})
+.go((next)=>{
+	setTimeout(()=>{
+		console.log('5func 100ms');
+		next();
+	},100);
+})
+.then((next)=>{
 	setTimeout(()=>{
 		console.log('then2');
+		next();
+
 	},0);
-	next();
 }).done((err)=>{
-	console.log('testGo1 OVER!');
+	console.log('testGo1 OVER!',err);
 })
